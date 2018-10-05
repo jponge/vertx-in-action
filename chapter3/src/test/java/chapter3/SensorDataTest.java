7d748b996a683806d8a78ee01e4883ec2e9fe0c8
Julien Ponge <julien.ponge@gmail.com>
2018-10-05 23:20:42 +0200

    Testing

---
+++ chapter3/src/test/java/chapter3/SensorDataTest.java
@@ -0,0 +1,31 @@
+package chapter3;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(VertxExtension.class)
+class SensorDataTest {
+
+  @Test
+  void testAverage(Vertx vertx, VertxTestContext ctx) {
+    EventBus bus = vertx.eventBus();
+    vertx.deployVerticle(new SensorData(), ctx.succeeding(id -> {
+      bus.publish("sensor.updates", new JsonObject()
+        .put("id", "a").put("temp", 20.0d));
+      bus.publish("sensor.updates", new JsonObject()
+        .put("id", "b").put("temp", 22.0d));
+      bus.send("sensor.average", "", ctx.succeeding(reply -> ctx.verify(() -> {
+        JsonObject json = (JsonObject) reply.body();
+        assertEquals(21.0d, (double) json.getDouble("average"));
+        ctx.completeNow();
+      })));
+    }));
+  }
+}
