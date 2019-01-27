1ab94d7e6ce0a632c87ea9a0f8472a0748e88541
Julien Ponge <julien.ponge@gmail.com>
2019-01-27 09:14:18 +0100

    Extract methods for readability

--- chapter5/src/main/java/chapter5/reactivex/CollectorService.java
+++ chapter5/src/main/java/chapter5/reactivex/CollectorService.java
@@ -29,26 +29,8 @@ public class CollectorService extends AbstractVerticle {
   }
 
   private void handleRequest(HttpServerRequest request) {
-    Single<HttpResponse<JsonObject>> r1 = fetchTemperature(3000);
-    Single<HttpResponse<JsonObject>> r2 = fetchTemperature(3001);
-    Single<HttpResponse<JsonObject>> r3 = fetchTemperature(3002);
-
-    Single<JsonObject> dataSingle = Single.zip(r1, r2, r3, (j1, j2, j3) -> {
-      JsonArray array = new JsonArray()
-        .add(j1.body())
-        .add(j2.body())
-        .add(j3.body());
-      return new JsonObject().put("data", array);
-    });
-
-    Single<JsonObject> snapshotSingle = dataSingle
-      .flatMap(json -> webClient
-        .post(4000, "localhost", "")
-        .expect(ResponsePredicate.SC_SUCCESS)
-        .rxSendJsonObject(json)
-        .flatMap(resp -> Single.just(json)));
-
-    snapshotSingle.subscribe(json -> {
+    Single<JsonObject> data = collectTemperatures();
+    sendToSnapshot(data).subscribe(json -> {
       request.response()
         .putHeader("Content-Type", "application/json")
         .end(json.encode());
@@ -58,11 +40,33 @@ public class CollectorService extends AbstractVerticle {
     });
   }
 
+  private Single<JsonObject> collectTemperatures() {
+    Single<HttpResponse<JsonObject>> r1 = fetchTemperature(3000);
+    Single<HttpResponse<JsonObject>> r2 = fetchTemperature(3001);
+    Single<HttpResponse<JsonObject>> r3 = fetchTemperature(3002);
+
+    return Single.zip(r1, r2, r3, (j1, j2, j3) -> {
+      JsonArray array = new JsonArray()
+        .add(j1.body())
+        .add(j2.body())
+        .add(j3.body());
+      return new JsonObject().put("data", array);
+    });
+  }
+
   private Single<HttpResponse<JsonObject>> fetchTemperature(int port) {
     return webClient
       .get(port, "localhost", "/")
       .expect(ResponsePredicate.SC_SUCCESS)
       .as(BodyCodec.jsonObject())
       .rxSend();
+  }
+
+  private Single<JsonObject> sendToSnapshot(Single<JsonObject> data) {
+    return data.flatMap(json -> webClient
+      .post(4000, "localhost", "")
+      .expect(ResponsePredicate.SC_SUCCESS)
+      .rxSendJsonObject(json)
+      .flatMap(resp -> Single.just(json)));
   }
 }
