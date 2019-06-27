d35e1330715db4719dbf5629a30cbec811d38035
Julien Ponge <julien.ponge@gmail.com>
2019-06-27 11:50:33 +0200

    Renamed to ingestion-service

--- part2-steps-challenge/settings.gradle.kts
+++ part2-steps-challenge/settings.gradle.kts
@@ -2,7 +2,11 @@ rootProject.name = "part2-steps-challenge"
 
 include("user-profile-service")
 include("activity-service")
-include("ingester")
-include("public-api")
-include("user-webapp")
+include("ingestion-service")
 include("congrats-service")
+include("event-stats-service")
+
+include("public-api")
+
+include("user-webapp")
+include("dashboard-webapp")
