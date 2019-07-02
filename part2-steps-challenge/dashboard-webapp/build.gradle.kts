4934646ea2f9397d2d47ac31c3085b85f04c13cc
Julien Ponge <julien.ponge@gmail.com>
2019-07-02 23:49:55 +0200

    Placeholder

---
+++ part2-steps-challenge/dashboard-webapp/build.gradle.kts
@@ -0,0 +1,40 @@
+//import com.moowork.gradle.node.yarn.YarnTask
+
+//plugins {
+//  id("com.moowork.node") version "1.3.1"
+//}
+
+dependencies {
+  val vertxVersion = extra["vertxVersion"]
+  val logbackClassicVersion = extra["logbackClassicVersion"]
+
+  implementation("io.vertx:vertx-rx-java2:$vertxVersion")
+  implementation("io.vertx:vertx-web:$vertxVersion")
+  implementation("ch.qos.logback:logback-classic:$logbackClassicVersion")
+}
+application {
+  mainClassName = "TBA"
+}
+
+//tasks.register<YarnTask>("buildVueApp") {
+//  inputs.file("package.json")
+//  inputs.file("yarn.lock")
+//  inputs.dir("src")
+//  outputs.dir("dist")
+//  args = listOf("build")
+//}
+//
+//tasks.register<Copy>("copyVueDist") {
+//  dependsOn("buildVueApp")
+//  from("$projectDir/dist")
+//  into("$projectDir/src/main/resources/webroot/assets")
+//}
+//
+//val processResources by tasks.named("processResources") {
+//  dependsOn("copyVueDist")
+//}
+//
+//val clean by tasks.named<Delete>("clean") {
+//  delete("$projectDir/dist")
+//  delete("$projectDir/src/main/resources/webroot/assets")
+//}
