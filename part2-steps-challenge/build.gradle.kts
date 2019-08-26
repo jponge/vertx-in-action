84718dab25513b8fec3c3ba1251ee317b619bf8e
Julien Ponge <julien.ponge@gmail.com>
2019-08-26 17:26:12 +0200

    Upgrades

--- part2-steps-challenge/build.gradle.kts
+++ part2-steps-challenge/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("com.github.ben-manes.versions") version "0.21.0"
+  id("com.github.ben-manes.versions") version "0.22.0"
   id("com.github.johnrengelman.shadow") version "5.1.0" apply false
   id("com.adarshr.test-logger") version "1.7.0" apply false
 }
@@ -13,12 +13,12 @@ subprojects {
     }
   }
 
-  project.project.extra["vertxVersion"] = "3.8.0"
+  project.project.extra["vertxVersion"] = "3.8.1"
   project.project.extra["junit5Version"] = "5.5.1"
   project.project.extra["restAssuredVersion"] = "4.0.0"
   project.project.extra["logbackClassicVersion"] = "1.2.3"
-  project.project.extra["assertjVersion"] = "3.12.2"
-  project.project.extra["testContainersVersion"] = "1.11.4"
+  project.project.extra["assertjVersion"] = "3.13.2"
+  project.project.extra["testContainersVersion"] = "1.12.0"
 
   apply(plugin = "java")
   apply(plugin = "application")
@@ -33,5 +33,5 @@ subprojects {
 
 tasks.wrapper {
   distributionType = Wrapper.DistributionType.ALL
-  gradleVersion = "5.5.1"
+  gradleVersion = "5.6"
 }
