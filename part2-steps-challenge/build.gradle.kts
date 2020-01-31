aa4dcff21b8872372f7e0fcccdfe8756a3ce9768
Julien Ponge <jponge@redhat.com>
2020-01-31 15:53:27 +0100

    Bump versions

--- part2-steps-challenge/build.gradle.kts
+++ part2-steps-challenge/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("com.github.ben-manes.versions") version "0.22.0"
-  id("com.github.johnrengelman.shadow") version "5.1.0" apply false
-  id("com.adarshr.test-logger") version "1.7.0" apply false
+  id("com.github.ben-manes.versions") version "0.27.0"
+  id("com.github.johnrengelman.shadow") version "5.2.0" apply false
+  id("com.adarshr.test-logger") version "2.0.0" apply false
 }
 
 subprojects {
@@ -14,11 +14,11 @@ subprojects {
   }
 
   project.project.extra["vertxVersion"] = "4.0.0-SNAPSHOT"
-  project.project.extra["junit5Version"] = "5.5.1"
-  project.project.extra["restAssuredVersion"] = "4.0.0"
+  project.project.extra["junit5Version"] = "5.6.0"
+  project.project.extra["restAssuredVersion"] = "4.2.0"
   project.project.extra["logbackClassicVersion"] = "1.2.3"
-  project.project.extra["assertjVersion"] = "3.13.2"
-  project.project.extra["testContainersVersion"] = "1.12.0"
+  project.project.extra["assertjVersion"] = "3.15.0"
+  project.project.extra["testContainersVersion"] = "1.12.5"
 
   apply(plugin = "java")
   apply(plugin = "application")
