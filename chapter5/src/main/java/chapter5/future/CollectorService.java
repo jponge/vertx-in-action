0eb1c9abea383f5a223d24caca79b61c3cec45bd
Julien Ponge <jponge@redhat.com>
2020-02-04 14:55:09 +0100

    Server start checks with a future

--- chapter5/src/main/java/chapter5/future/CollectorService.java
+++ chapter5/src/main/java/chapter5/future/CollectorService.java
@@ -3,6 +3,7 @@ package chapter5.future;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -21,11 +22,13 @@ public class CollectorService extends AbstractVerticle {
   private WebClient webClient;
 
   @Override
-  public void start() {
+  public void start(Promise<Void> promise) {
     webClient = WebClient.create(vertx);
     vertx.createHttpServer()
       .requestHandler(this::handleRequest)
-      .listen(8080);
+      .listen(8080)
+      .onFailure(promise::fail)
+      .onSuccess(ok -> System.out.println("http://localhost:8080/"));
   }
 
   private void handleRequest(HttpServerRequest request) {
