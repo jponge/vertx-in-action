2539a9a517757da2cd6ba0cb772245d62a4efccd
Julien Ponge <jponge@redhat.com>
2020-03-30 12:28:34 +0200

    Dependency upgrades

--- part2-steps-challenge/build.gradle.kts
+++ part2-steps-challenge/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("com.github.ben-manes.versions") version "0.27.0"
+  id("com.github.ben-manes.versions") version "0.28.0"
   id("com.github.johnrengelman.shadow") version "5.2.0" apply false
   id("com.adarshr.test-logger") version "2.0.0" apply false
 }
@@ -14,11 +14,11 @@ subprojects {
   }
 
   project.project.extra["vertxVersion"] = "4.0.0-SNAPSHOT"
-  project.project.extra["junit5Version"] = "5.6.0"
-  project.project.extra["restAssuredVersion"] = "4.2.0"
+  project.project.extra["junit5Version"] = "5.6.1"
+  project.project.extra["restAssuredVersion"] = "4.3.0"
   project.project.extra["logbackClassicVersion"] = "1.2.3"
   project.project.extra["assertjVersion"] = "3.15.0"
-  project.project.extra["testContainersVersion"] = "1.12.5"
+  project.project.extra["testContainersVersion"] = "1.13.0"
 
   apply(plugin = "java")
   apply(plugin = "application")
@@ -33,5 +33,5 @@ subprojects {
 
 tasks.wrapper {
   distributionType = Wrapper.DistributionType.ALL
-  gradleVersion = "6.1.1"
+  gradleVersion = "6.3"
 }
