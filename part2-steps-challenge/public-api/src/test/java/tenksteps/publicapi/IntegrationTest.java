afd6c97508f7b7c4408f27aac6c21d0e571e5f3a
Julien Ponge <jponge@redhat.com>
2020-04-08 21:47:39 +0200

    There are still spurious failures on this test

--- part2-steps-challenge/public-api/src/test/java/tenksteps/publicapi/IntegrationTest.java
+++ part2-steps-challenge/public-api/src/test/java/tenksteps/publicapi/IntegrationTest.java
@@ -10,7 +10,6 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.reactivex.core.RxHelper;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.pgclient.PgPool;
 import io.vertx.reactivex.sqlclient.Tuple;
@@ -27,7 +26,6 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 import static java.util.Arrays.asList;
@@ -74,7 +72,6 @@ class IntegrationTest {
       .ignoreElement()
       .andThen(vertx.rxDeployVerticle("tenksteps.activities.ActivityApiVerticle"))
       .ignoreElement()
-      .delay(5, TimeUnit.SECONDS, RxHelper.scheduler(vertx))
       .subscribe(testContext::completeNow, testContext::failNow);
   }
 
