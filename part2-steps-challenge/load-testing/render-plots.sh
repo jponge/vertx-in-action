e0accd85a76cc53808cf9ef97c0cb1f241b76e56
Julien Ponge <jponge@redhat.com>
2020-04-19 08:47:26 +0200

    Script to render all plots

---
+++ part2-steps-challenge/load-testing/render-plots.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p plots
+for f in data/hey-run*.csv; do
+    target=plots/plot-$(basename $f .csv)
+    echo $target
+    ./plot-hey-data.py $f $target.png 600
+    ./plot-hey-data.py $f $target.pdf 600
+done
