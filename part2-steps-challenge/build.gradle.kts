2bfea57ab78bdb02fc8df5ae6d81311e98fb536b
Julien Ponge <jponge@redhat.com>
2020-10-13 16:21:29 +0200

    Version upgrades
    Fixes TestContainer issues with Docker Desktop 2.4.0.0

--- part2-steps-challenge/build.gradle.kts
+++ part2-steps-challenge/build.gradle.kts
@@ -1,20 +1,20 @@
 import com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
 
 plugins {
-  id("com.github.ben-manes.versions") version "0.29.0"
+  id("com.github.ben-manes.versions") version "0.33.0"
   id("com.adarshr.test-logger") version "2.1.0"
-  id("com.github.johnrengelman.shadow") version "6.0.0" apply false
+  id("com.github.johnrengelman.shadow") version "6.1.0" apply false
   id("com.moowork.node") version "1.3.1" apply false
   id("org.gradle.test-retry") version "1.1.9" apply false
 }
 
 allprojects {
   extra["vertxVersion"] = if (project.hasProperty("vertxVersion")) project.property("vertxVersion") else "4.0.0.Beta3"
-  extra["junit5Version"] = "5.6.1"
+  extra["junit5Version"] = "5.7.0"
   extra["restAssuredVersion"] = "4.3.1"
   extra["logbackClassicVersion"] = "1.2.3"
-  extra["assertjVersion"] = "3.17.0"
-  extra["testContainersVersion"] = "1.14.3"
+  extra["assertjVersion"] = "3.17.2"
+  extra["testContainersVersion"] = "1.15.0-rc2"
 }
 
 subprojects {
